5c51dff10e241a4b9bd54b1d79628fee0b284222
Lemmah <lemayiannakolah@gmail.com>
2017-03-14 13:10:42 +0300

    Reinitialized the sliding mobile nav to close on click

--- js/init.js
+++ js/init.js
@@ -1,7 +1,14 @@
 (function($){
   $(function(){
-
     $('.button-collapse').sideNav();
+    $('.button-collapse').sideNav({
+      menuWidth: 300, // Default is 300
+      edge: 'left', // Choose the horizontal origin
+      closeOnClick: true, // Closes side-nav on <a> clicks, useful for Angular/Meteor
+      draggable: true // Choose whether you can drag to open on touch screens
+    }
+  );
+
     $('.parallax').parallax();
     $(document).ready(function(){
       $('.carousel').carousel();
