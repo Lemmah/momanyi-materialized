20572832795c4afa8733418d5b464dc1ecf042e1
Lemmah <lemayiannakolah@gmail.com>
2017-03-31 01:47:05 +0300

    Resized Navigation Drawer to 250px

--- js/init.js
+++ js/init.js
@@ -2,7 +2,7 @@
   $(function(){
     $('.button-collapse').sideNav();
     $('.button-collapse').sideNav({
-      menuWidth: 275, // Default is 300
+      menuWidth: 250, // Default is 300
       edge: 'left', // Choose the horizontal origin
       closeOnClick: true, // Closes side-nav on <a> clicks, useful for Angular/Meteor
       draggable: true // Choose whether you can drag to open on touch screens
